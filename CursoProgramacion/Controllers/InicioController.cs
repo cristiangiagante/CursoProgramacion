ee497060e38338f064b35dce04d3700a0168b581
Cristian Adrian Giagante <cristiangiagante@hotmail.com>
2017-11-03 01:40:45 -0300

    Agregar archivos de proyecto.

---
+++ CursoProgramacion/Controllers/InicioController.cs
@@ -0,0 +1,98 @@
+﻿using CursoDeProgramacion.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Web.Mvc;
+using System.Web.Script.Serialization;
+
+namespace CursoDeProgramacion.Controllers
+{
+    public class InicioController : Controller
+    {
+        public string DatabaseName { get; set; } = "bd.json";
+        public static string DbPath { get; set; }
+        // GET: Home
+        public ActionResult Index()
+        {
+            if(string.IsNullOrEmpty(DbPath))
+            {
+                DbPath = HttpContext.Server.MapPath($"~/jsonDb/{DatabaseName}");
+                if (!System.IO.File.Exists(DbPath))
+                {
+                    ClasesDiarias EmptyDB = new ClasesDiarias();
+                    EmptyDB.ListaClases = new List<ClaseDiaria>();
+                    SaveIntoJsonDB(EmptyDB);
+                }
+            }
+
+            var model = ObtenerJsonDB();
+            return View(model.ListaClases);
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var model = ObtenerJsonDB();
+            return View(model.ListaClases[id]);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ClaseDiaria clase)
+        {
+            var model = ObtenerJsonDB();
+            model.ListaClases[clase.Id] = clase;
+            SaveIntoJsonDB(model);
+            return View(clase);
+        }
+
+        public ActionResult Create(string passPhrase)
+        {
+            if (passPhrase == "unaclave")
+            {
+                return View("CrearClase");
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ClaseDiaria clase)
+        {
+
+            if (ModelState.IsValid)
+            {
+                AgregarIntoJsonDB(clase);
+            }
+            return RedirectToAction("Index");
+        }
+
+        private void SaveIntoJsonDB(ClasesDiarias list)
+        {
+            System.IO.File.Delete(DbPath);
+            var jsonDBObject = Json(list);
+            string result = new JavaScriptSerializer().Serialize(jsonDBObject.Data);
+            StreamWriter bd = System.IO.File.CreateText(DbPath);
+            bd.WriteLine(result);
+            bd.Close();
+        }
+
+        private void AgregarIntoJsonDB(ClaseDiaria clase)
+        {
+            var jsonDbStream = System.IO.File.OpenText(DbPath);
+            var jsonDbRaw = jsonDbStream.ReadToEnd();
+            jsonDbStream.Close();
+            var jsonDb = new JavaScriptSerializer().Deserialize<ClasesDiarias>(jsonDbRaw);
+            var cantidad = jsonDb.ListaClases.Count;
+            clase.Id += cantidad;
+            jsonDb.ListaClases.Add(clase);
+            SaveIntoJsonDB(jsonDb);
+        }
+
+        private ClasesDiarias ObtenerJsonDB()
+        {
+            var jsonDbStream = System.IO.File.OpenText(DbPath);
+            var jsonDbRaw = jsonDbStream.ReadToEnd();
+            jsonDbStream.Close();
+            var jsonDb = new JavaScriptSerializer().Deserialize<ClasesDiarias>(jsonDbRaw);
+            return jsonDb;
+        }
+    }
+}
---
+++ CursoProgramacion/Models/Clases.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CursoDeProgramacion.Models
+{
+    public class ClaseDiaria
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaPublicacion { get; set; }
+        public string VideoLink { get; set; }
+    }
+    public class ClasesDiarias
+    {
+        public List<ClaseDiaria> ListaClases { get; set; }
+    }
+}
